3cf5baefbd850d9f7d7892824d161d53f2e060f1
tamazbagdavadze <tamazbagdavadze@gmail.com>
2016-07-26 07:46:08 -0700

    fix js beta

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
  */
 
 
-function loadImages() {
+function loadImagesMainPage() {
     var images = [
         'images/mainServices/bg1.jpg',
         'images/mainServices/bg2.jpg',
@@ -24,57 +24,138 @@ function loadImages() {
     })
 }
 
-loadImages();
+function loadImagesForPages() {
+    var images = [
+        'images/mainServices/bg1.jpg',
+        'images/mainServices/bg2.jpg',
+        'images/mainServices/bg3.jpg'
+    ];
 
-$(function () {
+    var partners = ['baracuda', 'caucasus', 'magti', 'mikrotik', 'orientlogic', 'qnap', 'spam'];
 
-    var $erp = $("#erp");
-
-    var $erpBG = $erp.bgswitcher({
-        images: ["images/mainServices/1new.jpg", "images/mainServices/bg1.jpg"],
-        interval: 1000000,
-        duration: 300
+    partners.forEach(function (partner) {
+        images.push('images/partners/'+partner+'.png');
+        images.push('images/partners/'+partner+'_red.png');
     });
 
-    $erp.on("mouseenter", function() {
-        $erpBG.bgswitcher("next");
+    var img = document.createElement('img');
+
+    images.forEach(function (image) {
+        img.src = image;
+    })
+}
+
+
+
+if(window.isMain == true) {
+    loadImagesMainPage();
+
+    $(function () {
+
+        var $erp = $("#erp");
+
+        var $erpBG = $erp.bgswitcher({
+            images: ["images/mainServices/1new.jpg", "images/mainServices/bg1.jpg"],
+            interval: 1000000,
+            duration: 300
+        });
+
+        $erp.on("mouseenter", function () {
+            $erpBG.bgswitcher("next");
+        });
+
+        $erp.on("mouseleave", function () {
+            $erpBG.bgswitcher("next");
+        });
+
+
+        var $cloudMail = $("#cloudMail");
+
+        var $cloudMailBG = $cloudMail.bgswitcher({
+            images: ["images/mainServices/2new.jpg", "images/mainServices/bg2.jpg"],
+            interval: 1000000,
+            duration: 300
+        });
+
+        $cloudMail.on("mouseenter", function () {
+            $cloudMailBG.bgswitcher("next");
+        });
+
+        $cloudMail.on("mouseleave", function () {
+            $cloudMailBG.bgswitcher("next");
+        });
+
+
+        var $it = $("#it");
+
+        var $itBG = $it.bgswitcher({
+            images: ["images/mainServices/3new.jpg", "images/mainServices/bg3.jpg"],
+            interval: 1000000,
+            duration: 300
+        });
+
+        $it.on("mouseenter", function () {
+            $itBG.bgswitcher("next");
+        });
+
+        $it.on("mouseleave", function () {
+            $itBG.bgswitcher("next");
+        });
     });
+}
+else{
+    loadImagesForPages();
 
-    $erp.on("mouseleave", function() {
-        $erpBG.bgswitcher("next");
+    $(function () {
+
+        var $erp = $("#erp");
+
+        var $erpBG = $erp.bgswitcher({
+            images: ["images/servicesPage/1121.png", "images/servicesPage/112.png"],
+            interval: 1000000,
+            duration: 300
+        });
+
+        $erp.on("mouseenter", function() {
+            $erpBG.bgswitcher("next");
+        });
+
+        $erp.on("mouseleave", function() {
+            $erpBG.bgswitcher("next");
+        });
+
+
+        var $cloudMail = $("#cloudMail");
+
+        var $cloudMailBG = $cloudMail.bgswitcher({
+            images: ["images/servicesPage/1131.png", "images/servicesPage/113.png"],
+            interval: 1000000,
+            duration: 300
+        });
+
+        $cloudMail.on("mouseenter", function() {
+            $cloudMailBG.bgswitcher("next");
+        });
+
+        $cloudMail.on("mouseleave", function() {
+            $cloudMailBG.bgswitcher("next");
+        });
+
+
+        var $it = $("#it");
+
+        var $itBG = $it.bgswitcher({
+            images: ["images/servicesPage/1141.png", "images/servicesPage/114.png"],
+            interval: 1000000,
+            duration: 300
+        });
+
+        $it.on("mouseenter", function() {
+            $itBG.bgswitcher("next");
+        });
+
+        $it.on("mouseleave", function() {
+            $itBG.bgswitcher("next");
+        });
     });
-
-
-    var $cloudMail = $("#cloudMail");
-
-    var $cloudMailBG = $cloudMail.bgswitcher({
-        images: ["images/mainServices/2new.jpg", "images/mainServices/bg2.jpg"],
-        interval: 1000000,
-        duration: 300
-    });
-
-    $cloudMail.on("mouseenter", function() {
-        $cloudMailBG.bgswitcher("next");
-    });
-
-    $cloudMail.on("mouseleave", function() {
-        $cloudMailBG.bgswitcher("next");
-    });
-
-
-    var $it = $("#it");
-
-    var $itBG = $it.bgswitcher({
-        images: ["images/mainServices/3new.jpg", "images/mainServices/bg3.jpg"],
-        interval: 1000000,
-        duration: 300
-    });
-
-    $it.on("mouseenter", function() {
-        $itBG.bgswitcher("next");
-    });
-
-    $it.on("mouseleave", function() {
-        $itBG.bgswitcher("next");
-    });
-});
+}
